c552325caa18d65885771cc513001daed622167c
Akshay Singh <156520821+AkshaySingh2005@users.noreply.github.com>
2024-02-01 21:39:41 +0530

    Add files via upload

---
+++ A. Brick Wall.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main(){
+ 
+    int t;cin>>t;
+ 
+    while(t--){
+ 
+        int n,m;cin>>n>>m;
+ 
+        int ans = n*(m/2);
+ 
+        cout<<ans<<endl;
+    }
+}
