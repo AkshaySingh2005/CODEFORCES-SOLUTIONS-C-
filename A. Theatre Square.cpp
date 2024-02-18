25947ab4785986094dcf7b96b15914b41b3b9754
Akshay Singh <156520821+AkshaySingh2005@users.noreply.github.com>
2024-02-18 13:37:45 +0530

    Add files via upload

---
+++ A. Theatre Square.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+signed main(){
+fast
+
+int n,m,a;cin>>n>>m>>a;
+
+cout<<((n+a-1)/a)*((m+a-1)/a);
+
+}
